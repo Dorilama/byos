5a6dcc77d00068eb82c536775da27dfe8e4e5499
dorilama <assioma.xi@gmail.com>
2025-06-19 09:46:13 +0100

    feat: add specific usePeek for oby

--- src/oby/index.js
+++ src/oby/index.js
@@ -7,6 +7,7 @@ import { noop, createSignalFunctions } from "..";
 export const signalFunctions = createSignalFunctions({
   signal: (t) => $(t),
   computed: (fn) => $.memo(fn),
+  usePeek: (t) => [() => $.untrack(t), noop],
   toValue: (t) => ($.isObservable(t) ? t() : t),
   setValue: (s, t) => {
     s(t);
